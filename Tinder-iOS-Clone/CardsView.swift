b15725571462b1aeb626be7f8920945e0aa1ba3c
Arthur Jatoba <arthurjatoba93@gmail.com>
2021-04-13 17:00:11 -0300

    Created project, add images, created mainviewcontroller

---
+++ Tinder-iOS-Clone/CardsView.swift
@@ -0,0 +1,22 @@
+//
+//  CardsView.swift
+//  Tinder-iOS-Clone
+//
+//  Created by Arthur Octavio Jatobá Macedo Leite - ALE on 13/04/21.
+//
+
+import UIKit
+
+class CardsView: UIView {
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemPink
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
---
+++ Tinder-iOS-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tinder-iOS-Clone
+//
+//  Created by Arthur Octavio Jatobá Macedo Leite - ALE on 13/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
